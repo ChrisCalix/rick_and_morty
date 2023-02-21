083c659a7037c8b66cf14ea21ecfec4436aa47fc
christian.flores <christian.flores@GrupoMutual.Fi.Cr>
2023-02-21 17:56:55 -0400

    Initial Commit

---
+++ rick_and_morty/rick_and_mortyApp.swift
@@ -0,0 +1,17 @@
+//
+//  rick_and_mortyApp.swift
+//  rick_and_morty
+//
+//  Created by Sonic on 21/2/23.
+//
+
+import SwiftUI
+
+@main
+struct rick_and_mortyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
