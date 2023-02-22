1e112f7c559b94e2b263d95a4470e72f108ef17d
christian.flores <christian.flores@GrupoMutual.Fi.Cr>
2023-02-21 22:55:30 -0400

    validate the json decoder for success with valid json HTTP Response

--- rick_and_morty/RemoteFeedLoader.swift
+++ rick_and_morty/RemoteFeedLoader.swift
@@ -46,12 +46,12 @@ enum FeedCharacterMapper {
         let species: String
         let type: String
         let gender: String
-//        let origin: AnyObject?
-//        let location: AnyObject?
-        let image: String?
-        let episode: [String]?
-        let url: String?
-        let created: String?
+        let origin: Direction
+        let location: Direction
+        let image: String
+        let episode: [String]
+        let url: String
+        let created: String
         
         private enum CodingKeys: String, CodingKey {
             case id = "id"
@@ -60,27 +60,38 @@ enum FeedCharacterMapper {
             case species = "species"
             case type = "type"
             case gender = "gender"
-//            case origin = "origin"
-//            case location = "location"
+            case origin = "origin"
+            case location = "location"
             case image = "image"
             case episode = "episode"
             case url = "url"
             case created = "created"
+        }
+        
+        struct Direction: Decodable {
+            let name: String
+            let url: String
+            
+            private enum CodingKeys: String, CodingKey {
+                case name = "name"
+                case url = "url"
+            }
         }
     }
     
     private static let OK_200: Int = 200
     
     static func map(_ data: Data, response: HTTPURLResponse) -> RemoteFeedLoader.Result {
-        guard response.statusCode == OK_200, let _ = try? JSONDecoder().decode(Root.self, from: data) else {
+        guard response.statusCode == OK_200, let root = try? JSONDecoder().decode(Root.self, from: data) else {
             return .failure(RemoteFeedLoader.Error.invalidData)
         }
-        return .success([])
+        let character = FeedCharacter(id: root.id, name: root.name, status: root.status, species: root.species, type: root.type, gender: root.gender, origin: FeedCharacter.Direction(name: root.origin.name, url: root.origin.url), location: FeedCharacter.Direction(name: root.location.name, url: root.location.url), image: root.image, episode: root.episode, url: root.url, created: root.created)
+        return .success(character)
     }
 }
 
 protocol FeedLoader {
-    typealias Result = Swift.Result<[FeedCharacter], Error>
+    typealias Result = Swift.Result<FeedCharacter, Error>
     func load(completion: @escaping (Result) -> Void)
 }
 
@@ -90,7 +101,7 @@ protocol HTTPClient {
     func get(from url: URL, completion: @escaping (Result) -> Void)
 }
 
-struct FeedCharacter {
+struct FeedCharacter : Equatable {
     ///id    int    The id of the character.
     let id: Int
     ///name    string    The name of the character.
@@ -104,9 +115,9 @@ struct FeedCharacter {
     ///gender    string    The gender of the character ('Female', 'Male', 'Genderless' or 'unknown').
     let gender: String
     ///origin    object    Name and link to the character's origin location.
-    let origin: AnyObject
+    let origin: Direction
     ///location    object    Name and link to the character's last known location endpoint.
-    let location: AnyObject
+    let location: Direction
     ///image    string (url)    Link to the character's image. All images are 300x300px and most are medium shots or portraits since they are intended to be used as avatars.
     let image: String
     ///episode    array (urls)    List of episodes in which this character appeared.
@@ -115,4 +126,9 @@ struct FeedCharacter {
     let url: String
     ///created    string    Time at which the character was created in the database.
     let created: String
+    
+    struct Direction: Equatable {
+        let name: String
+        let url: String
+    }
 }
--- rick_and_mortyTests/LoadCharacterFromRemoteUseTestCase.swift
+++ rick_and_mortyTests/LoadCharacterFromRemoteUseTestCase.swift
@@ -12,25 +12,25 @@ class LoadCharacterFromRemoteUseTestCase: XCTestCase {
     
     func test_init_doesNotRequestDataFromURL() {
         let (_, client) = makeSUT()
-        
+
         XCTAssertTrue(client.requestedURLs.isEmpty)
     }
-    
+
     func test_loadTwice_requestDataFromURLTwice() {
         let url = URL(string: "https://rickandmortyapi.com/api/character/3")!
         let (sut, client) = makeSUT(url: url)
-        
+
         sut.load { _ in }
         sut.load { _ in }
-        
+
         XCTAssertEqual(client.requestedURLs, [url, url])
     }
-    
+
     func test_deliversConnectivityErrorOnClientError() {
         let (sut, client) = makeSUT()
-        
+
         let exp = expectation(description: "Wait for load completion")
-        
+
         sut.load { receivedResult in
             switch receivedResult {
             case let .failure(receivedError as RemoteFeedLoader.Error):
@@ -40,18 +40,18 @@ class LoadCharacterFromRemoteUseTestCase: XCTestCase {
             }
             exp.fulfill()
         }
-        
+
         let clientError = NSError(domain: "Test", code: 0)
         client.complete(with: clientError)
-        
+
         waitForExpectations(timeout: 1)
     }
-    
+
     func test_load_deliversInvalidDataErrorOnNon200HTTPResponse() {
         let (sut, client) = makeSUT()
-        
+
         let samples = [199, 201, 300, 400, 500]
-        
+
         samples.enumerated().forEach { index, code in
             let exp = expectation(description: "Wait for load completion")
             sut.load { receivedResult in
@@ -63,16 +63,16 @@ class LoadCharacterFromRemoteUseTestCase: XCTestCase {
                 }
                 exp.fulfill()
             }
-            let json = makeItemsJSON([])
+            let json = makeItemsJSON()
             client.complete(withStatusCode: code, data: json, at: index)
-            
+
             waitForExpectations(timeout: 1)
         }
     }
-    
+
     func test_load_deliversInvalidDataErrorOn200HTTPResponseWithinvalidJSON() {
         let (sut, client) = makeSUT()
-        
+
         let exp = expectation(description: "Wait for load completion")
         sut.load { receivedResult in
             switch receivedResult {
@@ -83,9 +83,33 @@ class LoadCharacterFromRemoteUseTestCase: XCTestCase {
             }
             exp.fulfill()
         }
-        
+
         let invalidJSON = Data("invalid JSON".utf8)
         client.complete(withStatusCode: 200, data: invalidJSON)
+
+        waitForExpectations(timeout: 1)
+    }
+    
+    func test_load_deliversSuccessWithNoItemsOn200HTTPResponseWithJSONItems() {
+        let (sut, client) = makeSUT()
+        
+        let secondCharacter = makeCharacter(id: 2, name: "Morty Smith", status: "Alive", species: "Human", gender: "Male", origin: FeedCharacter.Direction(name: "unknown", url: ""), location: FeedCharacter.Direction(name: "Citadel of Ricks", url: "https://rickandmortyapi.com/api/location/3"), image: "https://rickandmortyapi.com/api/character/avatar/2.jpeg", episode: ["https://rickandmortyapi.com/api/episode/1", "https://rickandmortyapi.com/api/episode/1"], url: "https://rickandmortyapi.com/api/character/2", created: "2017-11-04T18:50:21.651Z")
+        
+        let exp = expectation(description: "Wait for load completion")
+        sut.load { receivedResult in
+            switch receivedResult {
+            case let .failure(receivedError as RemoteFeedLoader.Error):
+                XCTFail("Expected Result instead")
+            case let .success(receivedCharacter):
+                XCTAssertEqual(receivedCharacter, secondCharacter.model)
+            default:
+                XCTFail("Expected result instead")
+            }
+            exp.fulfill()
+        }
+        
+        let json = makeItemsJSON(secondCharacter.json)
+        client.complete(withStatusCode: 200, data: json)
         
         waitForExpectations(timeout: 1)
     }
@@ -98,9 +122,38 @@ class LoadCharacterFromRemoteUseTestCase: XCTestCase {
         return (sut, client)
     }
     
-    private func makeItemsJSON(_ items: [[String: Any]]) -> Data {
-        let json = ["Characters": items]
-        return try! JSONSerialization.data(withJSONObject: json)
+    private func makeItemsJSON(_ character: [String: Any] = ["": ""]) -> Data {
+        return try! JSONSerialization.data(withJSONObject: character)
+    }
+    
+    private func makeCharacter(id: Int, name: String, status: String, species: String = "", type: String = "", gender: String = "", origin: FeedCharacter.Direction = FeedCharacter.Direction(name: "", url: ""), location: FeedCharacter.Direction = FeedCharacter.Direction(name: "", url: ""), image: String, episode: [String] = [], url: String, created: String = "") -> (model: FeedCharacter, json: [String: Any]) {
+        
+        let character = FeedCharacter(id: id, name: name, status: status, species: species, type: type, gender: gender, origin: origin, location: location, image: image, episode: episode, url: url, created: created)
+        
+        let jsonOrigin = [
+            "name": origin.name,
+            "url": origin.url
+        ]
+        let jsonLocation = [
+            "name": location.name,
+            "url": location.url
+        ]
+        let json : [String: Any]  = [
+            "id": id,
+            "name": name,
+            "status": status,
+            "species": species,
+            "type": type,
+            "gender": gender,
+            "origin": jsonOrigin,
+            "location": jsonLocation,
+            "image": image,
+            "episode": episode,
+            "url": url,
+            "created": created
+        ].compactMapValues{ $0 }
+        
+        return (character, json)
     }
     
     private class HTTPClientSpy: HTTPClient {
